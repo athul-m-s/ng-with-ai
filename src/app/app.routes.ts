c2b57d566c6709bc687f91aa3dcce12f028ae2bb
Athul ms <4thulms@gmail.com>
2026-03-27 12:21:55 +0530

    routing changes

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -1,3 +1,12 @@
 import { Routes } from '@angular/router';
 
-export const routes: Routes = [];
+export const routes: Routes = [
+  {
+    path: '',
+    loadComponent: () => import('./pages/home/home').then(m => m.HomeComponent)
+  },
+  {
+    path: '**',
+    loadComponent: () => import('./pages/error/error').then(m => m.ErrorComponent)
+  }
+];
---
+++ src/app/pages/home/home.ts
@@ -0,0 +1,65 @@
+import { Component, signal, inject } from '@angular/core';
+import { httpResource } from '@angular/common/http';
+import { AuthService } from '../../services/auth.service';
+import { NavbarComponent } from '../../components/navbar/navbar.component';
+import { LoginComponent } from '../../components/login/login.component';
+import { SignupComponent } from '../../components/signup/signup.component';
+import { Product } from '../../models/product/product.model';
+
+@Component({
+  selector: 'app-home',
+  imports: [NavbarComponent, LoginComponent, SignupComponent],
+  templateUrl: './home.html',
+  styleUrl: './home.css',
+})
+export class HomeComponent {
+  auth = inject(AuthService);
+
+  protected readonly title = signal('Siaara Store');
+  protected readonly shouldFetch = signal(false);
+
+  // UI State for Modals
+  protected readonly showLoginModal = signal(false);
+  protected readonly showSignupModal = signal(false);
+
+  protected readonly productsResource = httpResource<Product[]>(() =>
+    this.shouldFetch() && this.auth.isLoggedIn()
+      ? 'https://node-with-ai.azurewebsites.net/api/products'
+      : undefined,
+  );
+
+  loadProducts() {
+    if (this.auth.isLoggedIn()) {
+      this.shouldFetch.set(true);
+    } else {
+      this.openLogin();
+    }
+  }
+
+  // Modal controls
+  openLogin() {
+    if (this.auth.loginError()) this.auth.loginError.set(null);
+    this.showLoginModal.set(true);
+    this.showSignupModal.set(false);
+  }
+
+  closeLogin() {
+    this.showLoginModal.set(false);
+    if (this.shouldFetch() && this.auth.isLoggedIn()) {
+      this.productsResource.reload();
+    }
+  }
+
+  openSignup() {
+    if (this.auth.signupError()) this.auth.signupError.set(null);
+    this.showSignupModal.set(true);
+    this.showLoginModal.set(false);
+  }
+
+  closeSignup() {
+    this.showSignupModal.set(false);
+    if (this.shouldFetch() && this.auth.isLoggedIn()) {
+      this.productsResource.reload();
+    }
+  }
+}
